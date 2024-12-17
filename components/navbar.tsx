eb389f3dc4846c025391f0282cbae3c3cdae7619
Jakub Mazur <jakmaz.dev@icloud.com>
2024-12-17 18:02:02 +0100

    Prettier and responsive navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,36 +1,41 @@
 import Link from "next/link";
-import { BriefcaseBusiness } from "lucide-react";
+import { BriefcaseBusiness, Package, Truck } from "lucide-react";
 import packageJson from "@/package.json";
 
 export function Navbar() {
   const links = [
-    { href: "/delegations", label: "Delegations" },
-    { href: "/warehouse", label: "Magazine" },
+    { href: "/delegations", label: "Delegations", icon: Truck },
+    { href: "/warehouse", label: "Magazine", icon: Package },
   ];
 
   return (
-    <nav>
-      <div className="flex justify-between h-16">
-        <div className="flex">
-          <Link href="/" className=" flex items-center">
-            <BriefcaseBusiness className="h-8 w-8" />
-            <span className="ml-2 text-xl font-semibold">MyBusiness</span>
-            <span className="ml-1 text-sm text-gray-500">
-              v{packageJson.version}
-            </span>
-          </Link>
-        </div>
-        <div className="flex items-center">
-          <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-            {links.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className="border-transparent text-gray-800 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-              >
-                {link.label}
-              </Link>
-            ))}
+    <nav className="bg-white shadow-sm">
+      <div className="mx-auto ">
+        <div className="flex justify-between h-16">
+          <div className="flex items-center">
+            <Link href="/" className="flex items-end">
+              <BriefcaseBusiness className="h-8 w-8" />
+              <span className="ml-2 text-xl font-bold text-gray-900">
+                MyBusiness
+              </span>
+              <span className="ml-1 text-sm text-gray-500 bg-gray-100 px-2 py-1 rounded-full">
+                {packageJson.version}
+              </span>
+            </Link>
+          </div>
+          <div className="flex items-center">
+            <div className="flex gap-2 sm:gap-8">
+              {links.map((link) => (
+                <Link
+                  key={link.href}
+                  href={link.href}
+                  className="text-gray-700 hover:text-foreground hover:bg-slate-50 rounded-md text-sm font-medium transition-colors duration-150 ease-in-out flex items-center"
+                >
+                  <link.icon className="h-5 w-5 mr-2" />
+                  <span className="hidden sm:inline">{link.label}</span>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>
