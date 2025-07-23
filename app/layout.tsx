201b0189641ee212427f7fb0d7a30c5bada5b626
Jakub Mazur <jakmaz.dev@icloud.com>
2025-07-23 23:42:25 +0200

    feat: remove local font and change package name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "My Business",
@@ -26,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased p-8`}
-      >
+      <body className="antialiased p-8" >
         <div className="max-w-7xl mx-auto">
           <Navbar />
           <div className="py-10">{children}</div>
